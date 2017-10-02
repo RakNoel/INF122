0f32b4700ad22ee5b347e93e99bd034cfb94894e
RakNoel <Oskar.leir@gmail.com>
2017-10-02 16:55:11 +0000

    almost done

--- src/Oblig1.hs
+++ src/Oblig1.hs
@@ -10,31 +10,11 @@ parse :: String -> Ast
 parse = fst . parseExpr . words
 
 parseExpr :: [String] -> (Ast, [String])
-parseExpr ("+":xs) =
-    let (a,b) = parseExpr xs in
-        if null b then (a,b)
-        else let (x,z) = parseExpr b in
-            (Sum a x, z)
-
-parseExpr ("-":xs) =
-    let (a,b) = parseExpr xs in
-            (Min a, b)
-
-parseExpr ("*":xs) =
-    let (a,b) = parseExpr xs in
-        if null b then (a,b)
-        else let (x,z) = parseExpr b in
-            (Mul a x, z)
-
-parseExpr ("if":xs) =
-    let (x,a) = parseExpr xs in
-    let (y,b) = parseExpr a in
-    let (z,c) = parseExpr b in
-        (If x y z, c)
-
-parseExpr ("let":x:"=":xs) =
-    let (func, b) = getVarExpr xs in
-    let (c, d) = parseExpr b in
+parseExpr ("+":xs) = let (a,b) = parseExpr xs in if null b then (a,b) else let (x,z) = parseExpr b in (Sum a x, z)
+parseExpr ("-":xs) = let (a,b) = parseExpr xs in (Min a, b)
+parseExpr ("*":xs) = let (a,b) = parseExpr xs in if null b then (a,b) else let (x,z) = parseExpr b in (Mul a x, z)
+parseExpr ("if":xs) = let (x,a) = parseExpr xs in let (y,b) = parseExpr a in let (z,c) = parseExpr b in (If x y z, c)
+parseExpr ("let":x:"=":xs) = let (func, b) = getVarExpr xs in let (c, d) = parseExpr b in
     if isUpper (head x) && length x == 1 then (Let x (fst (parseExpr func)) c, d )
     else error "Illegal varriable"
 
@@ -50,28 +30,17 @@ getVarExpr ("in":xs) = ([],xs)
 getVarExpr (x:xs) = let (a,b) = getVarExpr xs in (x : a, b)
 
 evi::String -> Int
-evi xs = evival $ parse xs
-
-evival::Ast -> Int
-evival (Sum a b) = evival a + evival b
-evival (Mul a b) = evival a * evival b
-evival (Min a) = -evival a
-evival (If ev a b)
-    | evival ev == 0 = evival a
-    | otherwise = evival b
-evival (Nr a) = a
+evi xs = eval (parse xs) ([],0) (+) (*) (negate) (\x -> x) (\x -> (x == 0))
 
 evb::String -> Bool
-evb xs = evbval $ parse xs
+evb xs = eval (parse xs) ([],True) (||) (&&) not (\x -> (mod x 2 == 1)::Bool) (\x -> x)
 
-evbval:: Ast -> Bool
-evbval (Sum a b) = evbval a || evbval b
-evbval (Mul a b) = evbval a && evbval b
-evbval (Min a) = not $ evbval a
-evbval (If ev a b )
-    | evbval ev = evbval a
-    | otherwise = evbval b
-evbval (Nr a)
-    | a `mod` 2 == 0 = False
-    | otherwise = True
-
+{-      Var     Var-value     Sum              Mul              Min         Handling      If val       -}
+eval :: Ast -> (String, a) -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> (Int -> a) -> (a -> Bool) -> a
+eval (If var t f) (x,z) f1 f2 f3 e1 e2  = if e2 (eval var (x,z) f1 f2 f3 e1 e2)  then eval t (x,z) f1 f2 f3 e1 e2 else eval f (x,z) f1 f2 f3 e1 e2
+eval (Let st val ex) (x,z) f1 f2 f3 e1 e2 = eval ex (st, eval val (x,z) f1 f2 f3 e1 e2) f1 f2 f3 e1 e2
+eval (Var st) (x,z) _ _ _ _ _ = z
+eval (Sum a b) (x,z) f1 f2 f3 e1 e2 = f1 (eval a (x,z) f1 f2 f3 e1 e2) (eval b (x,z) f1 f2 f3 e1 e2)
+eval (Mul a b) (x,z) f1 f2 f3 e1 e2 = f2 (eval a (x,z) f1 f2 f3 e1 e2) (eval b (x,z) f1 f2 f3 e1 e2)
+eval (Min a) (x,z) f1 f2 f3 e1 e2 = f3 (eval a (x,z) f1 f2 f3 e1 e2)
+eval (Nr a) (_,_) _ _ _ e _ = e a
