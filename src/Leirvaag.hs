acc4e2c012217efa47fd17444c06e14549fc9f95
RakNoel <Oskar.leir@gmail.com>
2017-11-16 19:42:58 +0100

    finished

--- src/Leirvaag.hs
+++ src/Leirvaag.hs
@@ -1,10 +1,14 @@
-{- Oskar Leonhard Fretheim Leirvåg -}
+{-
+    Oskar Leonhard Fretheim Leirvåg
+    Spaceship:      s 2 3 b 3 3 30 1 1 4 1 5 2 5 3 5 4 4 4 3 4 2 4 1 3
+    Reach stable:   s 2 3 b 3 3 30 12 16 18 16 13 16 14 16 16 16 17 16 15 17 15 15
+    Pentadecathlon: s 2 3 b 3 3 30 11 15 12 15 13 16 13 14 19 15 14 15 16 15 15 15 17 15 20 15 18 14 18 16
+-}
 
 module Main where
 
 import Control.Concurrent
 import System.Exit
-import Data.List
 
 type Pos = (Int, Int)
 type Board = [Pos]
@@ -22,6 +26,7 @@ helpString rs rb = lineSplitter
         ++ "r name : \t read the rules and board from the file name \n"
         ++ "CR : \t\t step one generation forward. \n"
         ++ "l x : \t\t animates x steps forward \n"
+        ++ "cfg xs : \t\t reads paste as from file \n"
         ++ "q : \t\t quits the program."
         ++ lineSplitter
         ++ configToStr rs rb
@@ -38,7 +43,7 @@ configToStr rs rb = "Config: \n"
         xb = head rb ; yb = last rb
 
 lineSplitter :: String
-lineSplitter = "\n" ++ "==================================================" ++ "\n"
+lineSplitter = "\n" ++ "==================================================" ++ "\n\n"
 
 mergeSort:: (Ord a) => [a] -> [a]
 mergeSort [] = []
@@ -123,19 +128,21 @@ writeToFile f b s rs rb = writeFile f $ str ++ unwords (foldl (\x (a,o) -> show 
         xb = head rb ; yb = last rb
         str =   "s " ++ show xs ++ " " ++ show ys ++
                 " b " ++ show xb ++ " " ++ show yb ++
-                " n " ++ show s ++ " "
+                " " ++ show s ++ " "
 
 
 readFromFile :: String -> IO()
 readFromFile s = do
     str <- readFile s
     case words str of
-        ("s":x:y:"b":a:b:"n":n:xs) -> menu (listToBoard $ map read xs) (read n) [read x..read y] [read a.. read b]
-        ("b":a:b:"s":x:y:"n":n:xs) -> menu (listToBoard $ map read xs) (read n) [read x..read y] [read a.. read b]
+        ("s":x:y:"b":a:b:n:xs) -> menu (listToBoard $ map read xs) (read n) [read x..read y] [read a.. read b]
+        ("b":a:b:"s":x:y:n:xs) -> menu (listToBoard $ map read xs) (read n) [read x..read y] [read a.. read b]
+        _ -> do putStrLn "Format error in file" ; menu [] 10 [2,3] [3]
 
 listToBoard :: [Int] -> Board
 listToBoard [] = []
 listToBoard (x:y:xs) = (x,y) : listToBoard xs
+listToBoard _ = []
 
 
 menu :: Board -> Int -> [Int] -> [Int] -> IO()
@@ -144,26 +151,29 @@ menu b s rs rb = do
     command <- getLine
     cls
     case words command of
-        ("c":x:_) -> menu [] (read x) rs rb                                                                               -- [X]
-        ("n":x:y:_) -> menu (rmdupes $ (read x, read y):b) s rs rb                                                        -- [X]
-        ("d":x:y:_) -> menu (removeItem (read x, read y) b) s rs rb                                                       -- [X]
-        ("s":x:y:_) -> let r' = [read x .. read y] in do menu b s r' rb ; putStrLn $ configToStr r' rb                    -- [X]
-        ("b":x:y:_) -> let r' = [read x .. read y] in do menu b s rs r' ; putStrLn $ configToStr r' rb                    -- [X]
-        ("?":_) -> do putStr $ helpString rs rb ; menu b s rs rb                                                          -- [X]
-        ("w":name:_) -> do writeToFile name b s rs rb ; menu b s rs rb                                                    -- [X]
-        ("r":name:_) -> readFromFile name                                                                                 -- [X]
-        ("CR":_) -> do if b' == b && not (null b) then putStrLn "STABLE CONFIG" else putStrLn "" ; menu b' s rs rb        -- [X]
-        ("l":x:_) -> animate b s (read x) rs rb                                                                           -- [X]
-        ("q":_) -> exitSuccess                                                                                            -- [X]
-        _  -> menu b s rs rb                                                                                              -- [X]
+        ("c":x:_)    -> menu [] (read x) rs rb
+        ("n":x:y:_)  -> menu (rmdupes $ (read x, read y):b) s rs rb
+        ("d":x:y:_)  -> menu (removeItem (read x, read y) b) s rs rb
+        ("s":x:y:_)  -> let r' = [read x .. read y] in do putStrLn $ configToStr r' rb ; menu b s r' rb
+        ("b":x:y:_)  -> let r' = [read x .. read y] in do putStrLn $ configToStr r' rb ; menu b s rs r'
+        ("?":_)      -> do putStr $ helpString rs rb ; menu b s rs rb
+        ("w":name:_) -> do writeToFile name b s rs rb ; menu b s rs rb
+        ("r":name:_) -> readFromFile name
+        ("CR":_)     -> do if b' == b && not (null b) then putStrLn "STABLE CONFIG" else putStrLn "" ; menu b' s rs rb
+        ("l":x:_)    -> animate b s (read x) rs rb
+        ("cfg":"s":x:y:"b":a:z:n:xs) -> menu (listToBoard $ map read xs) (read n) [read x..read y] [read a.. read z]
+        ("q":_)      -> exitSuccess
+        _            -> do putStrLn "Unknow command or missing parameters, use ? for help"; menu b s rs rb
     where b' = nextGen b s rs rb
 
 animate :: Board -> Int -> Int -> [Int] -> [Int] -> IO()
-animate b s r rs rb = if r == 0 || (b' == b) then do cls ; menu b s rs rb else do
-    mapM_ putStr (boardToString b s)
-    threadDelay 128000
-    cls
-    animate (nextGen b s rs rb) s (r - 1) rs rb
+animate b s r rs rb =
+    if r == 0 || b' == b then do cls ; if b' == b then putStrLn "STABLE CONFIG" else putStrLn ""; menu b s rs rb
+    else do
+        mapM_ putStr (boardToString b s)
+        threadDelay 256000
+        cls
+        animate (nextGen b s rs rb) s (r - 1) rs rb
     where b' = nextGen b s rs rb
 
 main :: IO()
